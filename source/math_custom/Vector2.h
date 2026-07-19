f3e71dc4ed7e2d0495a144bf9241ffcc288f3ca9
GurgenAbrahamyan <gurgenabrahamyan22@gmail.com>
2026-07-19 13:18:52 +0400

    refactor(Vector2): add comparison operators and nearEqual

--- source/math_custom/Vector2.h
+++ source/math_custom/Vector2.h
@@ -31,11 +31,11 @@ public:
     Vector2& operator*=(float scalar) { x *= scalar; y *= scalar; return *this; }
     Vector2& operator/=(float scalar) { x /= scalar; y /= scalar; return *this; }
 
-    bool operator==(const Vector2 &) const { return false; /*return x == other.x && y == other.y;*/ }
-    bool operator!=(const Vector2&) const { return false; /* return !(*this == other);*/ }
+    bool operator==(const Vector2 & other) const { return  x == other.x && y == other.y;}
+    bool operator!=(const Vector2& other ) const { return  !(*this == other); }
 
-    bool nearEqual(const Vector2&, float) const {
-        return false; /*std::abs(x - other.x) < epsilon &&std::abs(y - other.y) < epsilon;*/
+    bool nearEqual(const Vector2& other, float epsilon) const {
+        return std::abs(x - other.x) < epsilon &&std::abs(y - other.y) < epsilon;
     }
 
 
