4ae05bb10ac290c5dd2c18614de180aedaed53da
GurgenAbrahamyan <gurgenabrahamyan22@gmail.com>
2026-07-19 14:18:01 +0400

    tests(Vector2): add mathematic function coverage

--- source/math_custom/Vector2.h
+++ source/math_custom/Vector2.h
@@ -3,6 +3,12 @@
 #include <cmath>
 #include <iostream>
 #include <cassert>
+
+enum class Direction {
+    CW,  // Clockwise
+    CCW  // Counter-Clockwise
+};
+
 class Vector2 {
 public:
     float x, y;
@@ -45,17 +51,22 @@ public:
     bool nearEqual(const Vector2& other, float epsilon = 1e-5f) const {
         return std::abs(x - other.x) < epsilon &&std::abs(y - other.y) < epsilon;
     }
-
-
   
     float length() const { return std::sqrt(x * x + y * y); }
+    float lengthSquared() const { return 0.0f;  /*       x *x + y *y;    */}
+
     Vector2 normalized() const {
         float len = length();
         return (len == 0) ? Vector2(0, 0) : Vector2(x / len, y / len);
     }
 
+    Vector2 perpendicular(Direction) const {
+        return Vector2{};
+    }
+
   
     float dot(const Vector2& other) const { return x * other.x + y * other.y; }
+    float cross(const Vector2& ) { return 0.0f; }
 
     void print() const { std::cout << "(" << x << ", " << y << ")\n"; }
 };
